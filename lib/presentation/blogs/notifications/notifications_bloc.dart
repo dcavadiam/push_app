f1fe761609a389138d68d182b6779d75dda5d17e
dcavadia <dcavadia@uninorte.edu.co>
2024-06-20 09:05:35 -0500

    local notifications configurated - Android file

--- lib/presentation/blogs/notifications/notifications_bloc.dart
+++ lib/presentation/blogs/notifications/notifications_bloc.dart
@@ -4,6 +4,7 @@ import 'package:equatable/equatable.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/config/local_notifications/local_notifications.dart';
 
 import 'package:push_app/domain/entities/push_message.dart';
 import 'package:push_app/firebase_options.dart';
@@ -102,6 +103,10 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       provisional: false,
       sound: true,
     );
+
+    // Solicitar permiso a las local notifications
+    await requestPermissionLocalNotifications();
+
     add(NotificationStatusChanged(settings.authorizationStatus));
   }
 
